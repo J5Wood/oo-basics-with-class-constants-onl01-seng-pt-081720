76b3976c7efd3ea702f13f8eb923e690b8771012
Jeremy Wood <js.wood206@gmail.com>
2020-09-03 04:46:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,10 +2,12 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  genres = []
+
   def initialize(title)
     @title = title
   end
-
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
